ed8466c12ba6a9259f8254846d3d20abe5626996
ralPeople <84584415+ralPeople@users.noreply.github.com>
2025-12-26 20:29:40 +0400

    Update index.py

--- index/index.py
+++ index/index.py
@@ -27,7 +27,7 @@ def index():
     ntp_time = datetime.fromtimestamp(response.tx_time, tz=timezone.utc)
     ntp_time = ntp_time + timedelta(hours=4)
     #is_correct_date = ntp_time.date() == datetime(2026, 1, 1).date()
-    is_correct_date = ntp_time.date() == datetime(2025, 12, 26).date()
+    is_correct_date = ntp_time.date() == datetime(2025, 12, 27).date()
 
     text = "Текст закрыт до Нового года, типо, да..."
     if is_correct_date:
